3fa18a0e913f3d9317b5eeb7f170f92b1e90e318
Ahmed7019 <ahmedabdallah7109@gmail.com>
2025-09-04 22:34:24 +0300

    FIX: modified the looks of the collections & cursor

--- components/Collections.jsx
+++ components/Collections.jsx
@@ -9,16 +9,20 @@ function Collections() {
         {arr.map((ele) => (
           <div
             key={ele}
-            className={`relative before:w-16 before:h-6 before:bg-yellow-100 before:-z-10 before:border before:border-yellow-300 before:rounded-r-4xl before:rounded-l-md before:-top-3 before:left-0 before:absolute bg-yellow-200 w-40 h-30 rounded-b-4xl rounded-tr-[60%] border border-yellow-300 shadow`}
+            className={`relative before:w-16 before:h-6 before:bg-yellow-100 before:-z-50 before:border before:border-yellow-300 before:rounded-r-4xl before:rounded-l-md before:-top-3 before:left-0 before:absolute bg-yellow-200 w-40 h-30 rounded-b-4xl rounded-tr-[60%] border border-yellow-300 shadow`}
           >
+            <div className="bg-white w-15 rotate-x-20 h-20 border rounded shadow absolute top-5 left-2  "></div>
+            <div className="bg-white w-15 rotate-x-20 h-20 border rounded shadow absolute top-5 left-8  rotate-6"></div>
+            <div className="bg-white w-15 rotate-x-20 h-20 border rounded shadow absolute top-5 left-10  rotate-12 overflow-hidden">
+              <p className="p-2 text-xs text-nowrap">web dev</p>
+            </div>
             <div
               className={`w-full h-15 relative top-15 bg-yellow-400 shadow-lg backdrop-saturate-100  rounded-b-4xl`}
             >
-              <div className="w-full flex items-center justify-between p-1">
-                <p className="text-xs text-neutral-900 bg-neutral-50/50 rounded-4xl border border-white shadow  backdrop-blur-md  p-1">
-                  Collection name
+              <div className="w-full flex items-center justify-between p-1 z-20">
+                <p className="text-sm text-neutral-900 bg-neutral-50/50 rounded-4xl border border-white shadow  backdrop-blur-md font-semibold p-1">
+                  Collection title
                 </p>
-                <p>...</p>
               </div>
             </div>
           </div>
--- components/CustomCursor.jsx
+++ components/CustomCursor.jsx
@@ -8,15 +8,28 @@ export default function CustomCursor() {
     <div
       id="cursor"
       className="
-        fixed top-0 left-0 w-4 h-4 
-        bg-neutral-800
-        rounded-full
-        pointer-events-none 
-        transform -translate-x-1/2 -translate-y-1/2 
-        transition-transform duration-[40ms] ease-linear
-        z-[9999]
-        hover:w-3
+      flex items-center justify-center 
+      fixed top-0 left-0
+      border border-neutral-600
+      pointer-events-none
+      w-8 h-8 rounded-full transform -translate-x-1/2 -translate-y-1/2
+      transition-transform duration-[100ms] ease-out
+      z-[9999]"
+    >
+      <div
+        id="cursor"
+        className="
+        fixed top-[50%] left-[50%]
+       w-2 h-2  
+      bg-neutral-800
+      rounded-full
+      pointer-events-none 
+      transform -translate-x-1/2 -translate-y-1/2 
+      transition-transform duration-[40ms] ease-linear
+      z-[9999]
+      hover:w-3
       "
-    />
+      />
+    </div>
   );
 }
