11efb544247286bfe4888604819e3aa914b05232
Ahmed7019 <ahmedabdallah7109@gmail.com>
2025-09-15 02:27:56 +0300

    FEAT DND kit : installed and built Draggable and Droppable components from dnd kit - Implemented the DND context to the notes - added Draggable component - added Droppable component - made the notes Draggable

--- components/notes.jsx
+++ components/notes.jsx
@@ -17,6 +17,8 @@ import { Button } from "./ui/button";
 import { useTransition } from "react";
 import { updateNoteAction } from "@/actions/actions";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "./Draggable";
 
 function Notes({ notes }) {
   const [isPending, startTransition] = useTransition();
@@ -24,108 +26,119 @@ function Notes({ notes }) {
   if (!notes || notes.length === 0) return <p>No notes to show </p>;
   return (
     <>
-      <div className="grid grid-cols-4 gap-2">
-        {notes.map((note) => (
-          <Dialog key={note._id}>
-            <DialogTrigger asChild>
-              <Card
-                className={
-                  "max-w-xs hover:ring hover:ring-yellow-900 bg-yellow-50"
-                }
-              >
-                <CardHeader>
-                  <CardTitle className="font-semibold text-lg">
-                    {note.title}
-                  </CardTitle>
-                </CardHeader>
-                <CardContent className={"text-sm text-accent-foreground"}>
-                  <p>
-                    {note.text.length >= 20
-                      ? note.text.slice(0, 20) + "..."
-                      : note.text}
-                  </p>
-                </CardContent>
-              </Card>
-            </DialogTrigger>
-            <DialogContent className="sm:w-[800px] z-100 absolute left-[25%] top-[20%]  bg-neutral-50 p-8 shadow-2xl border rounded-lg max-h-screen scroll-mr-8  overflow-y-auto">
-              <DialogHeader className={"flex items-center mb-4"}>
-                <DialogTitle>{note.title}</DialogTitle>
-              </DialogHeader>
+      <DndContext>
+        <div className="grid grid-cols-4 gap-2">
+          {notes.map((note) => (
+            <Dialog key={note._id}>
+              <DialogTrigger asChild>
+                <Draggable id={note._id}>
+                  <Card
+                    className={
+                      "max-w-xs hover:ring hover:ring-yellow-900 bg-yellow-50"
+                    }
+                  >
+                    <CardHeader>
+                      <CardTitle className="font-semibold text-lg">
+                        {note.title}
+                      </CardTitle>
+                    </CardHeader>
+                    <CardContent className={"text-sm text-accent-foreground"}>
+                      <p>
+                        {note.text.length >= 20
+                          ? note.text.slice(0, 20) + "..."
+                          : note.text}
+                      </p>
+                    </CardContent>
+                  </Card>
+                </Draggable>
+              </DialogTrigger>
+              <DialogContent className="sm:w-[800px] z-100 absolute left-[25%] top-[20%]  bg-neutral-50 p-8 shadow-2xl border rounded-lg max-h-screen scroll-mr-8  overflow-y-auto">
+                <DialogHeader className={"flex items-center mb-4"}>
+                  <DialogTitle>{note.title}</DialogTitle>
+                </DialogHeader>
 
-              {/* This is the popover settings button for the notes */}
-              <div className="flex justify-center items-center">
-                <div
-                  className="absolute top-4 right-4"
-                  aria-description="settings-of-note"
-                  title="options"
-                >
-                  <Popover>
-                    <PopoverTrigger asChild>
-                      <Button variant={"outline"} className={"text-black"}>
-                        ...
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className={"z-100"}>
-                      <div className="flex flex-col items-center gap-2">
-                        <Button>Add to collection</Button>
-                        <Button
-                          className={
-                            "text-red-500 bg-accent hover:bg-neutral-200 border shadow-inner"
-                          }
-                        >
-                          Delete
+                {/* This is the popover settings button for the notes */}
+                <div className="flex justify-center items-center">
+                  <div
+                    className="absolute top-4 right-4"
+                    aria-description="settings-of-note"
+                    title="options"
+                  >
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <Button variant={"outline"} className={"text-black"}>
+                          ...
                         </Button>
-                      </div>
-                    </PopoverContent>
-                  </Popover>
-                </div>
-
-                <form
-                  action={async (formData) => {
-                    startTransition(() => updateNoteAction(formData));
-                  }}
-                  className="flex flex-col gap-8"
-                >
-                  <input type="hidden" name="id" value={note._id.toString()} />
-                  <div className="grid gap-4">
-                    <div className="grid gap-3">
-                      <Label htmlFor="name-1">Title</Label>
-                      <Input
-                        id="title-1"
-                        name="title"
-                        defaultValue={note.title}
-                        className={
-                          " border-b-2 max-w-[400px] focus-visible:ring-0"
-                        }
-                      />
-                    </div>
-
-                    <div className="grid gap-3">
-                      <Textarea
-                        id="text-1"
-                        name="text"
-                        defaultValue={note.text}
-                        className={
-                          "min-h-80 w-[700px] border-none focus-visible:ring-0 shadow leading-relaxed font-semibold text-lg"
-                        }
-                      />
-                    </div>
+                      </PopoverTrigger>
+                      <PopoverContent className={"z-100"}>
+                        <div className="flex flex-col items-center gap-2">
+                          <Button>Add to collection</Button>
+                          <Button
+                            className={
+                              "text-red-500 bg-accent hover:bg-neutral-200 border shadow-inner"
+                            }
+                          >
+                            Delete
+                          </Button>
+                        </div>
+                      </PopoverContent>
+                    </Popover>
                   </div>
-                  <DialogFooter className="flex w-full justify-end">
-                    <Button
-                      className={"justify-end"}
-                      type="submit"
-                      disabled={isPending}
-                    >
-                      {isPending ? "Saving..." : "Save"}
-                    </Button>
-                  </DialogFooter>
-                </form>
-              </div>
-            </DialogContent>
-          </Dialog>
-        ))}
-      </div>
+
+                  <form
+                    action={async (formData) => {
+                      startTransition(() => updateNoteAction(formData));
+                    }}
+                    className="flex flex-col gap-8"
+                  >
+                    <input
+                      type="hidden"
+                      name="id"
+                      value={note._id.toString()}
+                    />
+                    <div className="grid gap-4">
+                      <div className="grid gap-3">
+                        <Label htmlFor="name-1">Title</Label>
+                        <Input
+                          id="title-1"
+                          name="title"
+                          defaultValue={note.title}
+                          className={
+                            " border-b-2 max-w-[400px] focus-visible:ring-0"
+                          }
+                        />
+                      </div>
+
+                      <div className="grid gap-3">
+                        <Textarea
+                          id="text-1"
+                          name="text"
+                          defaultValue={note.text}
+                          className={
+                            "min-h-80 w-[700px] border-none focus-visible:ring-0 shadow leading-relaxed font-semibold text-lg"
+                          }
+                        />
+                      </div>
+                    </div>
+                    <DialogFooter className="flex w-full justify-end">
+                      <Button
+                        className={"justify-end"}
+                        type="submit"
+                        disabled={isPending}
+                      >
+                        {isPending ? "Saving..." : "Save"}
+                      </Button>
+                    </DialogFooter>
+                  </form>
+                </div>
+              </DialogContent>
+            </Dialog>
+          ))}
+        </div>
+        {/* <Draggable>
+          <div>I'm draggable</div>
+        </Draggable> */}
+      </DndContext>
     </>
   );
 }
